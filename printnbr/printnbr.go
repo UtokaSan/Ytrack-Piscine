9160388c8c362378866029b0673ab1ec9ea23d89
Florian Boulbes <florian.boulbes@ynov.com>
2022-09-12 14:03:19 +0200

    [ADD] Files but doesn't work

--- printnbr/printnbr.go
+++ printnbr/printnbr.go
@@ -1,1 +1,22 @@
-package printnbr
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintNbr(n int) {
+	var nbNegOrPositif int = 49
+	if n < 0 {
+		nbNegOrPositif = 4549
+		z01.PrintRune('-')
+		n = n % 10 * nbNegOrPositif
+		z01.PrintRune(rune(n))
+	}
+	if n > 0 {
+		n = n % 10 * 1
+		z01.PrintRune(rune(n))
+	}
+}
+
+func main() {
+	PrintNbr(1234)
+	PrintNbr(-1234)
+}
